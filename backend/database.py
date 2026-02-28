cd6608127e040e8886d978772a4e0195ed43e4fc
Sarwar Altaf <contact@sarwaraltaf.in>
2026-02-28 14:56:34 +0530

    Connected to Railway MongoDB via environment variables

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 # Default local MongoDB connection if URI is not provided in env
-MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb://localhost:27017")
+MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb://mongo:YNRDRPcciIxZlVZJYVTMaeMwbXZYGqBj@mongodb.railway.internal:27017")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "globalpulse24")
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
